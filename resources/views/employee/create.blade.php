42e25295a6e0307ba8dedd84d88e78aed10f7d3d
andrew <iniandrewrp@gmail.com>
2023-05-15 21:31:47 +0700

    implement repopulating forms

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -55,19 +55,23 @@
                 <div class="row">
                     <div class="col-md-6 mb-3">
                         <label for="firstName" class="form-label">First Name</label>
-                        <input class="form-control" type="text" name="firstName" id="firstName" value="{{ old('firstName') }}" placeholder="Enter First Name">
+                        <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{ old('firstName') }}" placeholder="Enter First Name">
+                        @error('firstName') <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="lastName" class="form-label">Last Name</label>
-                        <input class="form-control" type="text" name="lastName" id="lastName" value="" placeholder="Enter Last Name">
+                        <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ old('lastName') }}" placeholder="Enter Last Name">
+                        @error('lastName') <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input class="form-control" type="text" name="email" id="email" value="" placeholder="Enter Email">
+                        <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ old('email') }}" placeholder="Enter Email">
+                        @error('email') <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="age" class="form-label">Age</label>
-                        <input class="form-control" type="text" name="age" id="age" value="" placeholder="Enter Age">
+                        <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ old('age') }}" placeholder="Enter Age">
+                        @error('age') <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                 </div>
                 <hr>
