412e3cf885be8dfaada6b254837192afb8700460
Chrisdion Andrew <iniandrewrp@gmail.com>
2023-07-01 21:15:14 +0700

    update title

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} &mdash;  @if(isset($pageTitle)) {{ $pageTitle }} @endif</title>
+    <title>{{ config('app.name', 'Laravel') }} @if(isset($pageTitle)) &mdash; {{ $pageTitle }} @endif</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
